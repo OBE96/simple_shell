ae296bfe90fda08a03ed750945361e31ec185775
0BE96 <Brightelo1@gmail.com>
2023-08-23 19:01:09 +0100

    updated

--- getinfo.c
+++ getinfo.c
@@ -1,28 +1,27 @@
 #include "shell.h"
 
 /**
- * clear_info - initialize info_t struct
- * @info: address of struct
+ * clear_info - initializes info_t struct
+ * @info: struct address
  */
 void clear_info(info_t *info)
 {
 	info->arg = NULL;
-	info->path = NULL;
 	info->argv = NULL;
+	info->path = NULL;
 	info->argc = 0;
 }
 
 /**
- * set_info - initialize info_t struct
- * @info: address of struct
+ * set_info - initializes info_t struct
+ * @info: struct address
  * @av: argument vector
  */
-
 void set_info(info_t *info, char **av)
 {
 	int i = 0;
-	info->fname = av[0];
 
+	info->fname = av[0];
 	if (info->arg)
 	{
 		info->argv = strtow(info->arg, " \t");
@@ -38,6 +37,7 @@ void set_info(info_t *info, char **av)
 		for (i = 0; info->argv && info->argv[i]; i++)
 			;
 		info->argc = i;
+
 		replace_alias(info);
 		replace_vars(info);
 	}
@@ -45,15 +45,14 @@ void set_info(info_t *info, char **av)
 
 /**
  * free_info - frees info_t struct fields
- * @info: address of struct
- * @all: all field
+ * @info: struct address
+ * @all: true if freeing all fields
  */
 void free_info(info_t *info, int all)
 {
 	ffree(info->argv);
-	info->path = NULL;
 	info->argv = NULL;
-
+	info->path = NULL;
 	if (all)
 	{
 		if (!info->cmd_buf)
