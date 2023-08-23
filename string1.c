6165685acf77e1ef37e331a009e0b88ff19ecbe5
0BE96 <Brightelo1@gmail.com>
2023-08-23 14:55:54 +0100

    simple_shell

---
+++ string1.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+/**
+ * _strcpy - copies a string
+ * @src: source
+ * @dest: destination
+ * Return: destination is pointed
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+/**
+ * _strdup - photocopy or duplicate a string
+ * @str: string 2 duplicate
+ * Return: pointer to duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+/**
+ * _puts - input string is printed
+ * @str: string to be printed
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _putchar - writes the char c to stdout
+ * @c: char to print
+ * Return: On success  return 1.
+ * On error, return -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
